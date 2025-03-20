1af74e59fdf29a9d395505ce2a7df2b417f2b519
cloudfox <snehansu.ghosh@gmail.com>
2025-03-20 22:52:35 +0530

    Prepared Flask app for deployment on Render

--- script.py
+++ script.py
@@ -15,4 +15,4 @@ def home():
     return f"<h1>✅ Factorial of {number} is {result}</h1>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
